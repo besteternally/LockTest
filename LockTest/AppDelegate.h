fd34eb19a4b3171b3cf2341e81a884feaaeab745
yuge <yuge@yugedeMac.local>
2018-11-04 17:11:02 -0800

    Initial Commit

---
+++ LockTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LockTest
+//
+//  Created by yuge on 2018/11/4.
+//  Copyright © 2018 yuge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LockTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LockTest
+//
+//  Created by yuge on 2018/11/4.
+//  Copyright © 2018 yuge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
